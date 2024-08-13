9c68ba0b087094e900aaaf440fec55b43ddf9210
anewla01 <andre.newland@tufts.edu>
2024-08-13 18:15:18 -0400

    BASH_01_Directory_Check

---
+++ knowledge_sync_bash_scripts/bash_01_directory_check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Purpose: check if a directory named backup exists in current directory, if not create it.
+
+curr_wd=$(pwd)
+expected_dir="${curr_wd}/backup" 
+if [[ -d "${expected_dir}" ]]
+then
+  echo "Directory: ${expected_dir} already exists"
+else
+  echo "Directory: ${expected_dir} does not exist, creating" 
+  mkdir "${expected_dir}"
+fi
