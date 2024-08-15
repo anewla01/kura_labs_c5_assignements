8c85ebfda46ada9d86075bc9b19944b85740386d
anewla01 <andre.newland@tufts.edu>
2024-08-14 21:04:28 -0400

    [Bash - Log Script]: added comment around where to pull data

--- inclass_excercises/bash_log_script_2024-08-14/analysis.sh
+++ inclass_excercises/bash_log_script_2024-08-14/analysis.sh
@@ -5,6 +5,7 @@
 # - understanding frequency of errors (and by IP)
 # Implementation Details
 # Code assumes that spaces are the key seperator, and that all special characters are escaped with quotes
+# Data pulled from:  curl -O https://c5databucket.s3.amazonaws.com/web-server-access-logs.log
 # Example Log Line: ,"54.36.149.41 - - [22/Jan/2019:03:56:14 +0330] ""GET /filter/27|13%20%D9%85%DA%AF%D8%A7%D9%BE%DB%8C%DA%A9%D8%B3%D9%84,27|%DA%A9%D9%85%D8%AA%D8%B1%20%D8%A7%D8%B2%205%20%D9%85%DA%AF%D8%A7%D9%BE%DB%8C%DA%A9%D8%B3%D9%84,p53 HTTP/1.1"" 200 30577 ""-"" ""Mozilla/5.0 (compatible; AhrefsBot/6.1; +http://ahrefs.com/robot/)"" ""-"""
 # Future work
 # - proper handling of function parameters
