ae98207d7589057d29147848c0aa8a1c4a439420
Andre C. Newland <andre.newland@tufts.edu>
2024-09-05 18:34:47 -0400

    PYTHON_02_Runtime_function updates (#11)

--- inclass_excercises/python_02_runtime_function/script.py
+++ inclass_excercises/python_02_runtime_function/script.py
@@ -10,15 +10,15 @@ def _float_cast(v):
         )
 
 
-def main(total_hours, down_hours):
+def calculate_uptime(total_hours, down_hours):
     down_hours = _float_cast(down_hours)
     total_hours = _float_cast(total_hours)
-    # return round(down_hours / total_hours * 100, 2)
+    return round((total_hours - down_hours) / total_hours * 100, 2)
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 3:
-        print("Percent down", main(*sys.argv[1:]), "%")
+        print("Percent uptime", calculate_uptime(*sys.argv[1:]), "%")
     else:
         raise RuntimeError(
             "Inappropriate number of params provided, please pass param 1 total_hours, and parmas 2 down_hours"
